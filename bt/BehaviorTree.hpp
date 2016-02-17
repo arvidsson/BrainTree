3fada44386200f0a2298a72a428780adb96475dc
arvidsson <arvidsson@gmail.com>
2016-02-17 14:36:14 +0100

    added shared blackboard

--- bt/BehaviorTree.hpp
+++ bt/BehaviorTree.hpp
@@ -9,14 +9,21 @@ namespace bt
 class BehaviorTree : public Node
 {
 public:
-    BehaviorTree();
-    Status Update();
-    void SetRoot(Node::Ptr node) { root = node; }
-    Blackboard::Ptr GetBlackboard() { return blackboard; }
+    BehaviorTree() : blackboard(std::make_shared<Blackboard>()) {}
+    BehaviorTree(const Node::Ptr &rootNode) : BehaviorTree(), root(rootNode) {}
+    BehaviorTree(const Blackboard::Ptr &shared) : BehaviorTree(), sharedBlackboard(shared) {}
+    
+    Status Update() { return root->Tick(); }
+    
+    void SetRoot(const Node::Ptr &node) { root = node; }
+    Blackboard::Ptr GetBlackboard() const { return blackboard; }
+    Blackboard::Ptr GetSharedBlackboard() const { return sharedBlackboard; }
+    void SetSharedBlackboard(const Blackboard::Ptr &shared) { sharedBlackboard = shared; }
     
 private:
     Node::Ptr root = nullptr;
     Blackboard::Ptr blackboard = nullptr;
+    Blackboard::Ptr sharedBlackboard = nullptr;
 };
 
 }

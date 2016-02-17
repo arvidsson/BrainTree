7fc4614c29d0809ebc06b2075d4a9ff28b59a928
arvidsson <arvidsson@gmail.com>
2016-02-17 14:41:19 +0100

    removed singleton and made blackboard header only

--- bt/Blackboard.hpp
+++ bt/Blackboard.hpp
@@ -10,31 +10,55 @@ namespace bt
 class Blackboard
 {
 public:
-    static Blackboard& GetInstance()
+    void SetBool(std::string key, bool value) { bools[key] = value; }
+    bool GetBool(std::string key)
     {
-        static Blackboard instance;
-        return instance;
+        if (bools.find(key) == bools.end()) {
+            bools[key] = false;
+        }
+        return bools[key];
     }
+    bool HasBool(std::string key) const { return bools.find(key) != bools.end(); }
 
-    void SetBool(std::string key, bool value);
-    bool GetBool(std::string key);
-    bool HasBool(std::string key) const;
+    void SetInt(std::string key, int value)  { ints[key] = value; }
+    int GetInt(std::string key)
+    {
+        if (ints.find(key) == ints.end()) {
+            ints[key] = 0;
+        }
+        return ints[key];
+    }
+    bool HasInt(std::string key) const  { return ints.find(key) != ints.end(); }
 
-    void SetInt(std::string key, int value);
-    int GetInt(std::string key);
-    bool HasInt(std::string key) const;
+    void SetFloat(std::string key, float value)  { floats[key] = value; }
+    float GetFloat(std::string key)
+    {
+        if (floats.find(key) == floats.end()) {
+            floats[key] = 0.0f;
+        }
+        return floats[key];
+    }
+    bool HasFloat(std::string key) const  { return floats.find(key) != floats.end(); }
 
-    void SetFloat(std::string key, float value);
-    float GetFloat(std::string key);
-    bool HasFloat(std::string key) const;
+    void SetDouble(std::string key, double value)  { doubles[key] = value; }
+    double GetDouble(std::string key)
+    {
+        if (doubles.find(key) == doubles.end()) {
+            doubles[key] = 0.0f;
+        }
+        return doubles[key];
+    }
+    bool HasDouble(std::string key) const  { return doubles.find(key) != doubles.end(); }
 
-    void SetDouble(std::string key, double value);
-    double GetDouble(std::string key);
-    bool HasDouble(std::string key) const;
-
-    void SetString(std::string key, std::string value);
-    std::string GetString(std::string key);
-    bool HasString(std::string key) const;
+    void SetString(std::string key, std::string value)  { strings[key] = value; }
+    std::string GetString(std::string key)
+    {
+        if (strings.find(key) == strings.end()) {
+            strings[key] = "";
+        }
+        return strings[key];
+    }
+    bool HasString(std::string key) const  { return strings.find(key) != strings.end(); }
 
     using Ptr = std::shared_ptr<Blackboard>;
 
